483408b6001b9043eae8b9f57147b546d586df6b
Jesse G <jessejames2015@web.de>
2021-05-16 22:11:48 +0200

    Working on the sell command.

--- src/main/java/com/erinnyen/econx/econCommands/sellCommand.java
+++ src/main/java/com/erinnyen/econx/econCommands/sellCommand.java
@@ -1,4 +1,35 @@
 package com.erinnyen.econx.econCommands;
 
-public class sellCommand {
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class sellCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        String header = ChatColor.LIGHT_PURPLE + "[Market]" + ChatColor.RESET;
+        //make Market command package
+
+        if(label.equalsIgnoreCase("send")){
+            if(!(sender instanceof Player)){
+                sender.sendMessage(header + ChatColor.DARK_RED +"You have to be a player to use this command!");
+                return false;
+            }
+            Player player = (Player) sender;
+
+            try{
+                double asked_price = Double.parseDouble(args[0]);
+
+            }catch(NumberFormatException e){
+                sender.sendMessage(header + ChatColor.DARK_RED + "Amount musst be a number!");
+                return false;
+            }
+
+
+        }
+        return false;
+    }
 }
