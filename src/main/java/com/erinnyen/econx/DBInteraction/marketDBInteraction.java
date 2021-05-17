eef47ae70ef243cc345d532095a801cebdd813a3
Jesse G <jessejames2015@web.de>
2021-05-17 19:32:54 +0200

    Added the marketDBInteraction for market Database Entries.

---
+++ src/main/java/com/erinnyen/econx/DBInteraction/marketDBInteraction.java
@@ -0,0 +1,16 @@
+package com.erinnyen.econx.DBInteraction;
+
+public class marketDBInteraction {
+
+
+    private final String uname;
+    private final String password;
+    private  final String url;
+
+    public marketDBInteraction(DBCredentials pDBcreds){
+        uname = pDBcreds.getUsername();
+        password = pDBcreds.getPassword();
+        url = pDBcreds.getUrl();
+
+    }
+}
