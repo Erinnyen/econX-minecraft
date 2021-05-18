1f1685af67ebec6b27eb41480963e61aae4b3666
Jesse G <jessejames2015@web.de>
2021-05-18 19:38:44 +0200

    Worked on the buy Command.

--- src/main/java/com/erinnyen/econx/econCommands/market/BuyCommand.java
+++ src/main/java/com/erinnyen/econx/econCommands/market/BuyCommand.java
@@ -1,14 +1,16 @@
 package com.erinnyen.econx.econCommands.market;
 
 import com.erinnyen.econx.DBInteraction.DBCredentials;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class BuyCommand implements CommandExecutor {
 
-
+    String header = ChatColor.LIGHT_PURPLE + "[Market]" + ChatColor.RESET;
     DBCredentials dbCreds;
 
     public BuyCommand(DBCredentials pDBcreds){
@@ -16,6 +18,35 @@ public class BuyCommand implements CommandExecutor {
     }
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        if(!label.equalsIgnoreCase("buy")){
+            return false;
+        }
+
+        if(!(sender instanceof Player)){
+            sender.sendMessage(header + ChatColor.DARK_RED +" You have to be a player to use this command!");
+            return false;
+        }
+        if(args.length != 1){
+            sender.sendMessage(header + ChatColor.DARK_RED + " Please specify the OrderID from the order you want to buy.");
+            return false;
+        }
+
+        int sell_order_id;
+
+        try{
+            sell_order_id = Integer.parseInt(args[0]);
+
+        }catch(NumberFormatException | IndexOutOfBoundsException e){
+            if(e instanceof IndexOutOfBoundsException){
+                // Checking which exception we're getting to send the corresponding error message.
+                sender.sendMessage(header + ChatColor.DARK_RED + " Please specify the OrderID from the order you want to buy.");
+            }else{
+                sender.sendMessage(header + ChatColor.DARK_RED + " OrderID must be a number!");
+            }
+            return false;
+        }
+
 
 
 
