d2a8d81f3db1ca7ae5b436db5b4f722b67fb8f3f
Jesse G <jessejames2015@web.de>
2021-05-20 11:04:41 +0200

    Added the getSellOrder() method to get an open SellOrder out of the database (it takes the order_id as identification)

--- src/main/java/com/erinnyen/econx/dbinteraction/Buy.java
+++ src/main/java/com/erinnyen/econx/dbinteraction/Buy.java
@@ -1,6 +1,12 @@
 package com.erinnyen.econx.dbinteraction;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.sql.*;
+import java.util.Map;
 
 public class Buy {
 
@@ -16,4 +22,41 @@ public class Buy {
 
 
     }
+    public void executeBuy(){}
+
+    private void getSellOrder(){
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+        try{
+
+            Connection conn = DriverManager.getConnection(dbCreds.getUrl(), dbCreds.getUsername(), dbCreds.getPassword());
+            PreparedStatement sellOrderQuery = conn.prepareStatement("SELECT price, seller_name, transaction_type, JSONString, order_id, timestamp  FROM sql_econx.open_sell_orders WHERE order_id = ?");
+            sellOrderQuery.setInt(1, sellOrderId);
+
+            ResultSet sellOrdersResult = sellOrderQuery.executeQuery();
+
+            if(sellOrdersResult.next()){
+
+                String json = sellOrdersResult.getString(4);
+                Gson gson = new Gson();
+
+                Map<String, Object> map = gson.fromJson(json, new TypeToken<Map<String, Object>>(){}.getType());
+                ItemStack soldItemObject = ItemStack.deserialize(map);
+
+                Sell theOrderYouWantToBuy = new Sell(dbCreds, sellOrdersResult.getDouble(1), sellOrdersResult.getString(2),
+                        sellOrdersResult.getInt(3), soldItemObject);
+                theOrderYouWantToBuy.setOrderId(sellOrdersResult.getInt(5));
+                theOrderYouWantToBuy.setOpenTimestamp(sellOrdersResult.getTimestamp(6));
+            }
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
 }
+
