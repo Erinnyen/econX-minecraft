331bfcf1dedfd5df112ab7c53fde9be4e6685ffa
Jesse G <jessejames2015@web.de>
2021-05-21 19:24:42 +0200

    Working on the MarketGUI.

--- src/main/java/com/erinnyen/econx/econcommands/market/MarketGUI.java
+++ src/main/java/com/erinnyen/econx/econcommands/market/MarketGUI.java
@@ -4,15 +4,18 @@ import com.erinnyen.econx.dbinteraction.DatabaseCredentials;
 import com.erinnyen.econx.dbinteraction.MarketDatabaseUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MarketGUI implements CommandExecutor {
 
@@ -40,9 +43,11 @@ public class MarketGUI implements CommandExecutor {
         Player player = (Player) sender;
         inv = Bukkit.createInventory(null, 27, ChatColor.LIGHT_PURPLE + "Items for Sale:");
 
-        MarketDatabaseUtil sellOrders = new MarketDatabaseUtil(dbCreds);
-        ArrayList<ItemStack> itemsForSale = sellOrders.getSellOrdersItemStacks(player.getName());
+        //MarketDatabaseUtil sellOrders = new MarketDatabaseUtil(dbCreds);
+        //ArrayList<ItemStack> itemsForSale = sellOrders.getSellOrdersItemStacks(player.getName());
 
+
+        /*
         try {
             for (int i = 0; i < itemsForSale.toArray().length; i++) {
                 ItemStack item = itemsForSale.get(i);
@@ -52,6 +57,22 @@ public class MarketGUI implements CommandExecutor {
             sender.sendMessage(header + ChatColor.RED + " There are no viewable offers.");
             return false;
         }
+
+
+         */
+
+
+        ItemStack item = new ItemStack(Material.COAL);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName("(Id: " + "3" + ") " + ChatColor.GOLD + "40.0" + "C");
+        List<String> lore = new ArrayList<String>();
+        lore.add(ChatColor.GRAY + "(" + "490" + "C per item)");
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+        item.setAmount(32);
+
+        inv.setItem(1, item);
+
 
 
         return true;
