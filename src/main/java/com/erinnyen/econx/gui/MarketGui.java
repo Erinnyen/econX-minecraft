b868697950ae061a63fd80bcfea97a89f9a09ae8
Jesse G <jessejames2015@web.de>
2021-05-24 14:35:14 +0200

    Fixed sell order duplicating bug.

--- src/main/java/com/erinnyen/econx/gui/MarketGui.java
+++ src/main/java/com/erinnyen/econx/gui/MarketGui.java
@@ -5,7 +5,6 @@ import com.erinnyen.econx.dbinteraction.MarketDatabaseUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.Item;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -17,21 +16,22 @@ public class MarketGui {
 
     public Inventory inv;
     private DatabaseCredentials dbCreds;
+    ItemStack exitBarrierBlock;
 
     public MarketGui(DatabaseCredentials pDBCreds){
         dbCreds = pDBCreds;
 
         inv = Bukkit.createInventory(null, 54, ChatColor.LIGHT_PURPLE + "Items for Sale:");
 
-        ItemStack item = new ItemStack(Material.BARRIER);
-        ItemMeta meta = item.getItemMeta();
+        exitBarrierBlock = new ItemStack(Material.BARRIER);
+        ItemMeta meta = exitBarrierBlock.getItemMeta();
         meta.setDisplayName(ChatColor.RED + "Close Shop");
         List<String> lore = new ArrayList<String>();
         lore.add(ChatColor.GRAY + "Click here to close the shop");
         meta.setLore(lore);
-        item.setItemMeta(meta);
+        exitBarrierBlock.setItemMeta(meta);
 
-        inv.setItem(53, item);
+        inv.setItem(53, exitBarrierBlock);
 
 
     }
@@ -39,6 +39,10 @@ public class MarketGui {
     public void addSellOrders(String pPlayerName){
 
         ArrayList<ItemStack> openSellOrderItemStacks = new MarketDatabaseUtil(dbCreds).getSellOrdersItemStacks(pPlayerName);
+        inv.clear();
+        // Adding the barrier block in the lower right corner again.
+        inv.setItem(53, exitBarrierBlock);
+
 
         for(Object item : openSellOrderItemStacks.toArray()){
             // I can only do the for loop with an Object so i have to cast ItemStack over it later.
