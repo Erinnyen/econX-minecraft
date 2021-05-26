32cdb16051fb15bd6166aac9a8a4181f91d3d044
Jesse G <jessejames2015@web.de>
2021-05-26 14:26:22 +0200

    Removed unnecessary import.

--- src/main/java/com/erinnyen/econx/listeners/InventoryClickListener.java
+++ src/main/java/com/erinnyen/econx/listeners/InventoryClickListener.java
@@ -9,7 +9,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
-import java.text.ParseException;
 
 public class InventoryClickListener implements Listener {
 
