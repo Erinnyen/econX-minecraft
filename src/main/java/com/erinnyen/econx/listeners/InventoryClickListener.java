c2d2830e528f6e6a9ba230dbe13d952df3c0599d
Jesse G <jessejames2015@web.de>
2021-05-26 14:24:41 +0200

    Fixed the NullPointerException produced by calling getLore() on getCurrentItem() and not getCurrentItem().getItemMeta()

--- src/main/java/com/erinnyen/econx/listeners/InventoryClickListener.java
+++ src/main/java/com/erinnyen/econx/listeners/InventoryClickListener.java
@@ -25,6 +25,8 @@ public class InventoryClickListener implements Listener {
 
     @EventHandler
     public void onClick(InventoryClickEvent event){
+
+        //Ignoring all the click events, that aren't within our shop
         if(!event.getInventory().equals(marketGui.inv)){
             return;
         }
@@ -37,6 +39,14 @@ public class InventoryClickListener implements Listener {
         if (event.getCurrentItem().getItemMeta().getDisplayName() == null){
             return;
         }
+        if(event.getCurrentItem().getItemMeta().getLore() == null){
+            return;
+        }
+
+        /*
+            Only if the criteria above aren't met, then we'll set the cancelled to true.
+            This means you can still move around items in your own inventory although you have the shop open.
+        */
 
         event.setCancelled(true);
         Player player = (Player) event.getWhoClicked();
@@ -46,18 +56,22 @@ public class InventoryClickListener implements Listener {
             return;
         }
 
-
+        /*
+            Using the try/catch here to catch the exceptions produced if the item doesn't
+            have the specific lore we specified in MarketGui.
+         */
         try {
-            int sellOrderId = Integer.parseInt(event.getCurrentItem().getLore().get(2));
+            int sellOrderId = Integer.parseInt(event.getCurrentItem().getItemMeta().getLore().get(2));
             Buy buy = new Buy(dbCreds, sellOrderId, player);
             String buyFeedback = buy.executeBuy();
-            if(buyFeedback != null){
+            if (buyFeedback != null) {
                 player.sendMessage(buyFeedback);
                 player.sendMessage(header + ChatColor.DARK_RED + " Something went wrong with executing the buy order.");
                 return;
             }
             marketGui.addSellOrders(player.getName());
             player.sendMessage(header + ChatColor.BOLD + " Transactions completed.");
+
 
         } catch (IndexOutOfBoundsException | NumberFormatException e) {
             return;
