84ce0b1f1639ce31c7dc071d76135245ae977205
Jesse G <jessejames2015@web.de>
2021-06-06 18:52:59 +0200

    Worked on display to user.

--- src/main/java/com/erinnyen/econx/util/Util.java
+++ src/main/java/com/erinnyen/econx/util/Util.java
@@ -4,13 +4,13 @@ import org.bukkit.ChatColor;
 
 public class Util {
 
-    public final static String MARKET = ChatColor.LIGHT_PURPLE + "[Market]" + ChatColor.RESET;
-    public final static String BANK =  ChatColor.LIGHT_PURPLE + "[Bank] " + ChatColor.RESET;
-    public final static String ECONX = ChatColor.AQUA + "[EconX]" + ChatColor.RESET;
+    public final static String MARKET_HEADER = ChatColor.LIGHT_PURPLE + "[Market] " + ChatColor.RESET;
+    public final static String BANK_HEADER =  ChatColor.LIGHT_PURPLE + "[Bank] " + ChatColor.RESET;
+    public final static String ECONX_HEADER = ChatColor.AQUA + "[EconX] " + ChatColor.RESET;
 
-    public final static String MARKET_ERROR = ChatColor.LIGHT_PURPLE + "[Market]" + ChatColor.DARK_RED + " Error: ";
-    public final static String BANK_ERROR = ChatColor.LIGHT_PURPLE + "[Bank] " + ChatColor.DARK_RED + " Error: ";
-    public final static String ECONX_ERROR = ChatColor.AQUA + "[EconX]"  + ChatColor.DARK_RED + " Error: ";
+    public final static String MARKET_ERR = ChatColor.LIGHT_PURPLE + "[Market]" + ChatColor.DARK_RED + " Error: ";
+    public final static String BANK_ERR = ChatColor.LIGHT_PURPLE + "[Bank] " + ChatColor.DARK_RED + " Error: ";
+    public final static String ECONX_ERR = ChatColor.AQUA + "[EconX]"  + ChatColor.DARK_RED + " Error: ";
 
     public final static String PERM_ERR = ChatColor.DARK_RED + "You don't have the permissions to do that.";
 
